689778906f33e13523fed120711a91e7f72465ab
Samuel Davis <samueljakdavis@gmail.com>
2025-02-14 10:43:54 -0500

    click-to-move

--- player.gd
+++ player.gd
@@ -1,12 +1,21 @@
 extends CharacterBody2D
 
 
-@export var speed := 160.0
+@export var speed: float = 160.0
+@export var position_margin: float = 5.0
 
-@onready var target := position
-var moving := false
+@onready var target_position: Vector2 = global_position
+
 
 func _physics_process(_delta: float) -> void:
-	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = direction * speed
+	if global_position.distance_to(target_position) < position_margin:
+		velocity = Vector2.ZERO
+
 	move_and_slide()
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and Input.is_action_just_pressed("ui_accept"):
+		target_position = event.position
+		var direction: Vector2 = (target_position - global_position).normalized()
+		velocity = direction * speed
