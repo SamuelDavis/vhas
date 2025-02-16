0c2ce3e8739e001afd54da87d8ac0512ce4c9710
Samuel Davis <samueljakdavis@gmail.com>
2025-02-16 15:30:38 -0500

    integrate second floor; stylize characters

--- game.gd
+++ game.gd
@@ -1,37 +1,70 @@
 extends Node2D
 
 
-@onready var player: CharacterBody2D = $Player
-@onready var hiding_spots: Area2D = $World/HidingSpots
+@onready var jessica: CharacterBody2D = $World/Jessica
+@onready var first_hiding_spots: Area2D = $World/FirstFloor/HidingSpots
+@onready var second_hiding_spots: Area2D = $World/SecondFloor/HidingSpots
 @onready var ray: RayCast2D = $RayCast2D
 @onready var label: Label = $Label
 @onready var stairs: Area2D = $World/Stairs
+@onready var first_floor: Node2D = $World/FirstFloor
+@onready var second_floor: Node2D = $World/SecondFloor
+@onready var active_floor: Node2D = $World/FirstFloor
 
 var hiding_spot: String = ""
 
 
 func _ready() -> void:
-	hiding_spots.input_event.connect(
-		func (_viewport: Viewport, event: InputEvent, shape_idx: int):
-			if (event is InputEventMouseButton
-			and event.pressed):
-				_check_hiding_spot(event, shape_idx)
-	)
+	for spots: Area2D in [first_hiding_spots, second_hiding_spots]:
+		spots.input_event.connect(
+			func (_viewport: Viewport, event: InputEvent, shape_idx: int):
+				if (event is InputEventMouseButton
+				and event.pressed):
+					_check_hiding_spot(event, shape_idx, spots)
+		)
 	_set_hiding_spot()
-	stairs.body_exited.connect(func(_body: CharacterBody2D):
-		print("figure out which direction and thus which layer to show")
-	)
+	stairs.body_entered.connect(_toggle_level)
+	_toggle_level(jessica)
+	_toggle_level(jessica)
+
+func _toggle_level(body) -> void:
+	if body != jessica: return
+
+	for child in active_floor.get_children():
+		if child is TileMapLayer:
+			child.collision_enabled = false
+		if child is Area2D:
+			child.input_pickable = false
+
+	if second_floor.visible:
+		second_floor.visible = false
+		active_floor = first_floor
+		first_floor.modulate = Color(1.0, 1.0, 1.0, 1)
+	else:
+		second_floor.visible = true 
+		active_floor = second_floor
+		first_floor.modulate = Color(0.5, 0.5, 0.5, 1)
+
+	for child in active_floor.get_children():
+		if child is TileMapLayer:
+			child.collision_enabled = true 
+		if child is Area2D:
+			child.input_pickable = true
 
 
-func _check_hiding_spot(_event: InputEventMouseButton, idx: int) -> void:
-	var spot: CollisionShape2D = hiding_spots.get_children()[idx]
+
+func _check_hiding_spot(_event: InputEventMouseButton, idx: int, spots: Area2D) -> void:
+	var spot: CollisionShape2D = spots.get_children()[idx]
 	var params := PhysicsRayQueryParameters2D.new()
-	params.from = player.global_position
+	params.from = jessica.global_position
 	params.to = spot.global_position
 	params.collide_with_bodies = false
 	params.collide_with_areas = true
-	var res := get_world_2d().direct_space_state.intersect_ray(params)
-	var dist := player.global_position.distance_to(res.position)
+	var res: Dictionary = get_world_2d().direct_space_state.intersect_ray(params)
+
+	if not res.has("position"): return
+
+	var dist := jessica.global_position.distance_to(res.position)
 	if dist < 10 and label.global_position == Vector2.ZERO:
 		if spot.name == hiding_spot:
 			_spawn_label(res.position, "Here!")
@@ -59,4 +92,6 @@ func _spawn_label(at: Vector2, text: String, distance: float = 36.0, time: float
 
 
 func _set_hiding_spot() -> void:
-	hiding_spot = hiding_spots.get_children().pick_random().name
+	var first_floor_spot = first_hiding_spots.get_children().pick_random().name
+	var second_floor_spot = second_hiding_spots.get_children().pick_random().name
+	hiding_spot = first_floor_spot if randi() % 2 == 0 else second_floor_spot
